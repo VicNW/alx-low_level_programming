76ce6f8dc10de6953bc2a5aa002f3374b6e989f6
Nwosu Victor <nwosuvictor54@gmail.com>
2022-04-07 20:33:07 +0400

    error fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,9 @@
  * Return: 0 if exited properly, non-zero otherwise
  */
 
- int main<void>
+ int main(void)
  {
- 	printf("with proper grammar, but the outcome is a piece of art");
+ 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	
-	return(0);
+	return (0);
 }
