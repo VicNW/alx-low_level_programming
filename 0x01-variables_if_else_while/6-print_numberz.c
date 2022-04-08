373e9ac0d982dfcd05d99f8bb7e71bce06664858
Nwosu Victor <nwosuvictor54@gmail.com>
2022-04-09 01:02:33 +0400

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - This code prints numbers 1 - 10
+* Return: always 0, successful.
+*/
+int main(void)
+{
+	int x = 0;
+
+	for (x = 0; x < 10; x++)
+	{
+	putchar("%d", x);
+	};
+	putchar("\n");
+	return (0);
+}
