a5472e59d53cdf3fe94d64e4560b0ac321738e8c
Nwosu Victor <nwosuvictor54@gmail.com>
2022-04-09 02:02:43 +0400

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,22 @@
 */
 int main(void)
 {
-  	int i, e;
+
+	int i, e;
 
 	i = 48;
 	e = 48;
 
 	while (e < 58)
 	{
+
 		i = 48;
 		while (i < 58)
 		{
+
 			if (e != i && e < i)
 			{
+
 				putchar(e);
 				putchar(i);
 				if (i == 57 && e == 56)
