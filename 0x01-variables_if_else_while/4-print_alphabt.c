bb157138cc8376423fd2181bbc61cf576d517687
Nwosu Victor <nwosuvictor54@gmail.com>
2022-04-09 00:39:10 +0400

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,15 +6,15 @@
 */
 int main(void)
 {
-	char ch ='a';
+	char ch = 'a';
 
 	while (ch <= 'z')
 	{
 	if (ch != 'e' && ch != 'q')
 	{
 	putchar(ch);
-	ch++;
 	}
+	ch++;
 	}
 	putchar('\n');
 	return (0);
