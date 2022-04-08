854999bef0af9c2f9e26ea641f474eca5beadd9f
Nwosu Victor <nwosuvictor54@gmail.com>
2022-04-09 00:48:09 +0400

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - This code prints numbers 1 - 10
+* Return: always 0, successful.
+*/
+int main(void)
+{
+	int x = 0;
+
+	for (x = 0; x <= 10; x++)
+	{
+	printf("%d\n", x);
+	}
+	return (0);
+}
