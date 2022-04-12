df008b70ddd402908c1dc156502d37c49d01850c
Nwosu Victor <nwosuvictor54@gmail.com>
2022-04-12 15:30:44 +0400

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	putchar(char task0);
+	putchar(char task0[]);
 	putchar("\n");
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
 /**
 * main - this is the header file for the 0x02-functions_nested_loops
 * Return: 0, successful
 */
 
+int _putchar(char c);
+
 int main(void)
 {
-	char task0[] = "_putchar";
-
-	int _putchar(char c);
+	char task0[] = "utchar";
+	printf("_p%s", task0[]);
 	return (0);
 }
