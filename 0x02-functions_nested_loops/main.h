7c4395c3be4247a30223a5f3f978cf6fc5bc2029
Nwosu Victor <nwosuvictor54@gmail.com>
2022-04-14 14:44:55 +0400

    debugging and more_functions_nested_loops task

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,6 +14,5 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-
 #endif
 
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _isupper - evalue if letter is uppercase .
+ * _isdigit - if is a digit
  *@c: print int
  * Return: Always 0.
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-if (c >= 'A' && c <= 'Z')
+if (c >= 48 && c <= 57)
 {
 return (1);
 }
@@ -16,5 +16,4 @@ else
 {
 return (0);
 }
-
 }
