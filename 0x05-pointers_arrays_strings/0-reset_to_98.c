379d4f5f7110276f8f98ffc87e70d25cde570696
Nwosu Victor <nwosuvictor54@gmail.com>
2022-04-20 17:31:34 +0400

    Task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,11 @@
 /**
 * reset_to_98 - this function takes a pointer to an int as parameter,
 * and updates the value it points to 98.
+* n - integer
 * Return: void, there is no return value.
 */
 
 void reset_to_98(int *n)
 {
-	
 	*n = 98;
 }
