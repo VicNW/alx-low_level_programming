f6aaa381462bb646fe401001a508ead8ea692250
Nwosu Victor <nwosuvictor54@gmail.com>
2022-04-20 20:34:55 +0400

    Task3_9

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strlen - find the lenght of a string
+* @s: pointer to the string to chack
+* Return: void
+*/
+
+nit _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	i++;
+
+	return (i)
+}
