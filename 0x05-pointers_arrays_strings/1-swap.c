1da79f57246b67fa07a7de6961165cbce1a63bcd
Nwosu Victor <nwosuvictor54@gmail.com>
2022-04-20 19:26:03 +0400

    Task1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
- #include "main.h"
+#include "main.h"
 
 /**
-* swap_int - swap variable values
-* @a: pointer
-* @b: pointer
-* Return: void
+* swap_int - swap variable values
+* @a: pointer 1
+* @b: pointer 2
+* Return: void
 */
 
-void swap_int(int *a, int *b)
-{ 
-	*a += *b; 
-	*b = *a - *b;
-	*a = *a - *b;
+void swap_int(int *a, int *b)
+{
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
 }
