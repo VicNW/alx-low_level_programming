7a9df587edeeda71cb0cca5e1074853ba08b1424
Nwosu Victor <nwosuvictor54@gmail.com>
2022-04-26 16:51:05 +0400

    task2-4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _strpbrk - searches a string for any of a set of bytes
+* @s: pointer to string
+* @accept: accepted character
+* Return: pointer to string
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i
+
+	if (*s(i) = 'accept')
+	{
+		s[i]
+		break
+	}
+	else 
+	{
+		_putchar(0);
+	}
+	return (s);
+}
