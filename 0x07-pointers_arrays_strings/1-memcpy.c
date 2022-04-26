8eb6d493495c59b4fe4db332d1a3a1ff6da8f1c6
Nwosu Victor <nwosuvictor54@gmail.com>
2022-04-26 15:38:59 +0400

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _memcpy - copies mempry area
+* @dest: pointer to destination array
+* @src: pointer to source array
+* @n: number of iterations
+* Return: character array
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+}
