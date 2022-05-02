992ef2e68b6c2b8ec8ed580bbea20117fd5dfb7f
Nwosu Victor <nwosuvictor54@gmail.com>
2022-05-02 20:18:09 +0400

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - this function prints all arguments it receives.
+* @argc: length of arguments passed in the function
+* @argv: array of strings passed in the command line
+* Return: zero, successful
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
