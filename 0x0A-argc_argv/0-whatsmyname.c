cd21f2e4d473e8ec8504214dbbf8ce1110a62fae
Nwosu Victor <nwosuvictor54@gmail.com>
2022-05-02 19:19:35 +0400

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - this function prints th efile name.
+* @argc: length of arguments passed in the function
+* @argv: array of strings passed in the command line
+* Return: zero, successful
+*/
+
+int main(int argc, char* argv)
+{
+	printf("%s", argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Practice on passing function atrguments
