bb2df53e2259e6300e2d28c0a30c7607f8f467e3
Nwosu Victor <nwosuvictor54@gmail.com>
2022-05-04 16:23:36 +0400

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,9 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	p = malloc(size * sizeof(char));
 	if (p == NULL)
-		return (NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
