b957aabf130aa21737259925d88ae7ab84f4822f
Nwosu Victor <nwosuvictor54@gmail.com>
2022-05-09 15:55:24 +0400

    task3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,6 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_H_
 #define _3_FUNCTION_LIKE_MACRO_H_
 
-#define ABS(x) \
-#if x >= 0
-return x 
-#else 
-return (-)*(x)
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif /* _3_FUNCTION_LIKE_MACRO_H_ */
