7291ae67d610badd2d603195f1c9b9827ef81602
Nwosu Victor <nwosuvictor54@gmail.com>
2022-05-10 02:24:21 +0400

    task1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,14 +1,14 @@
 #include "dog.h"
 
 /**
-* init_dog - initializes a structure of type dog
-* @d: pointer to structure
-* @name: pointer to the name of the dog
-* @age: age of dog
-* @owner: pointer to owner of dog
-*
-* Return: void
-*/
+ * init_dog - initializes a structure of type dog
+ * @d: pointer to structure
+ * @name: pointer to the name of the dog
+ * @age: age of dog
+ * @owner: pointer to owner of dog
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
