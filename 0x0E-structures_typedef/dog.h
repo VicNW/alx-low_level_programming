b76940ad8f91e2d595cd24de6774166f83b8308a
Nwosu Victor <nwosuvictor54@gmail.com>
2022-05-10 02:29:57 +0400

    task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,14 +19,17 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for struct dog
-  */
-  typedef struct dog dog_t;
+* dog_t - Typedef for struct dog
+*/
 
-  int _putchar(char);
-  void init_dog(struct dog *d, char *name, float age, char *owner);
-  void print_dog(struct dog *d);
-  dog_t *new_dog(char *name, float age, char *owner);
-  void free_dog(dog_t *d);
+/**
+* typedef struct dog dog_t;
+*/
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* _DOG_H_ */
