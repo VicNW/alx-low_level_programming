48cc7591cefa8771ee065674def97f0eff286ee4
Nwosu Victor <nwosuvictor54@gmail.com>
2022-05-11 14:09:55 +0400

    task1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array != NULL && size != NULL & action != NULL)
+	if (array & action)
 	{
 		for (i = 0; i < size; i++)
 		{
