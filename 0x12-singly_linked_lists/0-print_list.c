296147fbe135a644ce716f0d47805306538f2b65
Nwosu Victor <nwosuvictor54@gmail.com>
2022-05-20 00:44:09 +0400

    task2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to the start of the list
- *
- * Return: number of nodes in the list
- */
+* print_list - prints all the elements of a list_t list
+* @h: pointer to the start of the list
+*
+* Return: number of nodes in the list
+*/
 size_t print_list(const list_t *h)
 {
 	const list_t *tmp;
