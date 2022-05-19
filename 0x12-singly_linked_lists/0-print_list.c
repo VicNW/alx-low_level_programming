0e2c28eea7249157dbb060c8cf8924e3017b5359
Nwosu Victor <nwosuvictor54@gmail.com>
2022-05-19 23:48:04 +0400

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,20 +3,20 @@
 
 /**
 * print_list - prints all the elements of a list_t list
-* @h: string input
+* @h: pointer to the start of the list
 *
 * Return: the number of nodes
 */
 
 size_t print_list(const list_t *h)
 {
-	int n;
+	unsigned int n;
 	const list_t *temp;
 	
 	temp = h;
 	for (n = 0 ; temp; n++)
 	{
-		printf("[%d] %s\n", temp->len, temp->str);
+		printf("[%u] %s\n", temp->len, temp->str);
 		temp = temp->next;
 	}
 	return (n);
