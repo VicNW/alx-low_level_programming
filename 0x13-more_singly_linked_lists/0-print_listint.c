6c637c401de4d985bc8af5b4cc1572dd29efb4f2
Nwosu Victor <nwosuvictor54@gmail.com>
2022-05-24 21:33:22 +0400

    more singly listed list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,16 +1,21 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_listint -  prints all the elements of a listint_t list.
+ * @h: list to print
+ *
+ * Return: number of elements in the list
+ */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *temp;
-	temp = h;
-	int count;
+	size_t count = 0;
 
-	for (count = 0; temp; count++)
+	while (h != NULL)
 	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 	}
 	return (count);
 }
