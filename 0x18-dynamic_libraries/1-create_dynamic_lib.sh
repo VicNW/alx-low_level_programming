70fd55ac643e87151e230e1fb393784fdcf08ca7
Nwosu Victor <nwosuvictor54@gmail.com>
2022-07-11 22:01:47 +0400

    shell script

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c -fPIC *.c || gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
